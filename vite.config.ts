d8dc00412d49f27ab4c773076c0d259cf17cded3
Emanuel Medeiros <emanuel.medeiros@live.com>
2023-11-12 19:59:23 -0300

    [edit]: change some configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: './',
+  server: {
+    port: 8500,
+    //host: '0.0.0.0', // Use if  need to expose
+    strictPort: true,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      ':': fileURLToPath(new URL('./src/modules/', import.meta.url))
     }
   }
 })
