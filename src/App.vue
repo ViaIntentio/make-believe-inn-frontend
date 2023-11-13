2b20fec3f6a7b20f513398a9e70103984660e301
Emanuel Medeiros <emanuel.medeiros@live.com>
2023-11-13 13:22:02 -0300

    [edit]: check auth user

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import BaseLayout from '@/app/layout/layout.base.vue';
+import { useAuthStore } from ':/auth/store/auth-store';
+
+const auth = useAuthStore()
 </script>
 
 <template>
+  	<div>
+		<span v-if="auth.user">
+			{{ auth.user.name }}
+			<button @click="auth.logout">logout</button>
+		</span>
+		<span v-else>
+			<router-link :to="{name: 'auth.login'}">login</router-link>
+		</span>
+	</div>
   <BaseLayout>
     <RouterView />
   </BaseLayout>
